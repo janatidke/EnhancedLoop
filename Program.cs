cf0742dc1cc21afce780b6989cfc39409eab08c8
Janabai Tukaram Tidke <janatidke2407@gmail.com>
2021-08-31 21:50:18 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace EnhancedLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Print the element of  array:");
+
+            int[] array = new int[] { 1, 2, 3, 4, 5, 6, 7 };
+
+
+            foreach (int items in array)
+            {
+                Console.WriteLine(items);
+            }
+        }
+    }
+}
